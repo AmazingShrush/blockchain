197768e8d4b32cbfbe8d8bd949bb11fa2a9d6f32
Shrusti <shrusti.sangodkar11@gmail.com>
2023-05-01 16:51:03 +0100

    Final code with comments

--- Incentive_Mechanism.sol
+++ Incentive_Mechanism.sol
@@ -1,27 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.0;
+/// @title A peer to peer smart contract with an incentive mechanism
+/// @author Shrusti S. Sangodkar
+/// @dev All function calls are currently implemented without side effects
+/// @custom:experimental This is an experimental contract.
 
 contract P2PEnergy{
-    struct Prosumer{
-        address ID;
-        int energyStatus;
-        uint256 balance;
-        bool isRegistered;
-        uint noOfTransactions;
-        uint  rewardPoints;
+     
+    uint energyPrice = 1 ether;
+    uint fee = 10; // The service fee charged for every transaction
+
+    // Struct representing a prosumer
+    struct Prosumer {
+        address ID; //Stores the address of the prosumer
+        int energyStatus; // Energy status can be positive or negative, indicating the amount of energy the prosumer can supply or needs to buy
+        uint256 balance; // Prosumer's account balance
+        bool isRegistered; // true if prosumer is registered, else false
+        
+        //Necessary for implementing the Incentive Mechanism
+        uint noOfTransactions; // Number of transactions the prosumer has participated in
+        uint rewardPoints; // Number of reward points earned by the prosumer
     }
-        uint energyPrice = 1 ether;
-        uint fee = 10;
-    mapping(address => Prosumer) internal prosumers;
-    address[] internal buyerQueue;
-    address[] internal sellerQueue;
-    uint256 public etherRewards;
+     
 
+    mapping(address => Prosumer) internal prosumers; // Mapping from prosumer addresses to their Prosumer struct
+    address[] internal buyerQueue; // Queue of buyers waiting to buy energy
+    address[] internal sellerQueue; // Queue of sellers waiting to sell energy
+    uint256 internal etherRewards; // Total amount of ether rewards accumulated, initially 0
 
+    // Function to register a new prosumer
     function registerProsumer(address _prosumerAddress) internal{
         prosumers[_prosumerAddress] = Prosumer(_prosumerAddress,0,0,true,0,0);
     }
+
+    // Function to remove an element from an array
     function removeAtIndex(uint index, address[] storage array) internal {
         require(index < array.length, "Index out of bounds");
         for (uint i = index; i < array.length - 1; i++) {
@@ -29,35 +42,43 @@ contract P2PEnergy{
         }
         array.pop();
     }
-    // function giveRewardTokens(address _prosumer) internal{
-    //     prosumers[_prosumer].token = 1;
-    //     prosumers[_prosumer].noOfTransactions = 0;
-    // }
+
+    // Function for a seller to sell energy to a buyer
     function sellEnergy(address _prosumer,int energyUnits) internal {
         bool foundBuyer = false;
+
+        //Incentive mechanism to increment number of transactions and give rewards if number of transactions is greater than specified amount
         prosumers[_prosumer].noOfTransactions += 1;
-        if(prosumers[_prosumer].noOfTransactions > 3){
+        if(prosumers[_prosumer].noOfTransactions > 4){
                     prosumers[_prosumer].rewardPoints = 1;
                     prosumers[_prosumer].noOfTransactions = 0;
         }
+
+        //Iterating through buyer queue to search for buyers
         for(uint i=0; i<buyerQueue.length;i++){
             if(-1*prosumers[buyerQueue[i]].energyStatus <= energyUnits){
-                int y=energyUnits;
+                int actualTransferableEnergy=energyUnits;
+
+                //Condition to calculate transferable energy is it is less than the available energy units
                 if(-1*prosumers[buyerQueue[i]].energyStatus < energyUnits){
 
-                    int x=energyUnits + prosumers[buyerQueue[i]].energyStatus;
-                     y= energyUnits - x;
+                    int differenceInEnergy=energyUnits + prosumers[buyerQueue[i]].energyStatus;
+                     actualTransferableEnergy= energyUnits - differenceInEnergy;
+
+                     //Store surplus energy in the seller queue
                      Prosumer storage sender = prosumers[_prosumer];
-                     sender.energyStatus = x;
+                     sender.energyStatus = differenceInEnergy;
                      sellerQueue.push(_prosumer);
                 }
 
-                prosumers[buyerQueue[i]].energyStatus += y;
+                prosumers[buyerQueue[i]].energyStatus += actualTransferableEnergy;
 
-                // Trading ethers with service fee
-                uint totalCost = uint(y) * energyPrice;
+                 // Calculate the total cost of the transaction and the service fee
+                uint totalCost = uint(actualTransferableEnergy) * energyPrice;
                 uint feeAmount = (totalCost * fee) / 100;
                 uint finalCost = totalCost + feeAmount;
+
+                // Update the balances of the buyer and the seller, and the total ether rewards
                 prosumers[buyerQueue[i]].balance -= finalCost;
                 prosumers[_prosumer].balance += finalCost - feeAmount;
                 etherRewards += feeAmount;
@@ -70,28 +91,36 @@ contract P2PEnergy{
             }
         }
         if(!foundBuyer){
+            // if no buyer is found, the prosumer is added to the buyer queue
             Prosumer storage sender = prosumers[_prosumer];
             sender.energyStatus += energyUnits;
             sellerQueue.push(_prosumer);
         }
     }
-//function to buy energy
+
+    //function to buy energy
     function buyEnergy(address _prosumer,int energyUnits) internal {
 
         bool foundSeller = false;
+
+        //Incentive mechanism to increment number of transactions and give rewards if number of transactions is greater than specified amount
         prosumers[_prosumer].noOfTransactions += 1;
-        if(prosumers[_prosumer].noOfTransactions > 3){
+        if(prosumers[_prosumer].noOfTransactions > 4){
                      prosumers[_prosumer].rewardPoints = 1;
                      prosumers[_prosumer].noOfTransactions = 0;
         }
+
+        //Iterating through seller queue to search for sellers
         for (uint i = 0; i < sellerQueue.length; i++) {
             if (prosumers[sellerQueue[i]].energyStatus >= -1 * energyUnits) {
                 prosumers[sellerQueue[i]].energyStatus += energyUnits;
-                //prosumers[_prosumer].energyStatus -= energyUnits;
 
+                 // Calculate the total cost of the transaction and the service fee
                 uint totalCost = uint(-energyUnits) * energyPrice;
                 uint feeAmount = (totalCost * fee) / 100;
                 uint finalCost = totalCost + feeAmount;
+
+                // Update the balances of the buyer and the seller, and the total ether rewards
                 prosumers[sellerQueue[i]].balance += finalCost - feeAmount;
                 prosumers[_prosumer].balance -= finalCost;
                 etherRewards += feeAmount;
@@ -99,13 +128,11 @@ contract P2PEnergy{
                 if(prosumers[sellerQueue[i]].energyStatus == 0){
                     removeAtIndex(i,sellerQueue);
                 }
-                
-                 
-
                 break;
             }
         }
         if (!foundSeller) {
+            // if no seller is found, the prosumer is added to the buyer queue
             Prosumer storage sender = prosumers[_prosumer];
             sender.energyStatus += energyUnits;
             buyerQueue.push(_prosumer);
@@ -116,42 +143,54 @@ contract P2PEnergy{
 }
 
 contract MainContract is P2PEnergy{
+
+    //Modifier to check if a prosumer is registered
     modifier prosumerIsRegistered(){
         require(prosumers[msg.sender].isRegistered == true,"The prosumer is not registered");
         _;
     }
 
+    //Modifier to check whether a prosumer is not registered
     modifier notRegisteredProsumer(){
         require(prosumers[msg.sender].isRegistered == false, " The prosumer is already registered");
         _;
     }
 
+    // Modifier to check whether the minimum amount of funds are present in prosumer's account in order to purchase energy units
     modifier checkFunds(int energy){
         uint energyUnits;
         
         if(energy < 0){
         energyUnits= uint(-energy);
         int requestsSent = prosumers[msg.sender].energyStatus;
-        uint256 amountAvailable=prosumers[msg.sender].balance;
+       
 
+        //Calculating cost based on fees, energyPrice and required energy units
         uint totalCost = energyUnits * energyPrice;
         uint feeAmount = (totalCost * fee) / 100;
         uint finalCost = totalCost + feeAmount;
-        //uint256 newAmountCheck = energyUnits * 1 ether + energyUnits * 1 ether * 0.10;
-        uint256 amountAvailableRequest=prosumers[msg.sender].balance - uint(-requestsSent) * 1 ether;
+
+        uint256 amountAvailable=prosumers[msg.sender].balance;
         require(amountAvailable >= finalCost, " Insufficient balance in prosumer's account");
-        require(amountAvailableRequest >= energyUnits * 1 ether, "Pending request amount exceeds the available balance in prosumer's");
+
+        //Checking past energy requests and seeing if the pending request amount exceeds the available balance in prosumer's balance
+        uint pasttotalCost = uint(-requestsSent) * energyPrice;
+        uint pastfeeAmount = (pasttotalCost * fee) / 100;
+        uint pastRequestCost = pasttotalCost + pastfeeAmount;
+
+        uint256 amountAvailableRequest=prosumers[msg.sender].balance - pastRequestCost;
+        require(amountAvailableRequest >= finalCost, "Pending request amount exceeds the available balance in prosumer's");
         }
         _;
     }
 
-
+    // Function to register a prosumer
     function register() public notRegisteredProsumer{
         registerProsumer(msg.sender);
     }
 
-    function sendEnergyRequest(int energy) public prosumerIsRegistered checkFunds(energy){
-       
+    //Function to send a buyer or a seller request 
+    function sendEnergyRequest(int energy) public prosumerIsRegistered checkFunds(energy){ 
         if(energy > 0){
             sellEnergy(msg.sender,energy);
         }else{
@@ -164,23 +203,16 @@ contract MainContract is P2PEnergy{
     }
 
     //function to check current balance
-    function checkCurrentBalance() public prosumerIsRegistered view returns (uint) {
-        return prosumers[msg.sender].balance/ 10**18;
+    function checkCurrentBalance() public prosumerIsRegistered view returns (uint256) {
+        return prosumers[msg.sender].balance;
     }
 
-    function checkTransactions() public prosumerIsRegistered view returns (uint noOfTransactions){
-        return prosumers[msg.sender].noOfTransactions;
-    }
-
+    //This is a part of the incentive mechanism, in order to check whether any reward points were collected 
     function checkRewardPoints() public prosumerIsRegistered view returns (uint){
         return prosumers[msg.sender].rewardPoints;
     }
 
-    //Delete this 
-    function checkP2PContractRewards()public view returns (uint){
-        return etherRewards/ 10**18;
-    }
-
+    //Function to exhange reward points for ethers, in this case 0.5 ether is given as an incentive
     function exchangeRewardPointsForEthers(uint _rewardPoints) public prosumerIsRegistered{
         require(prosumers[msg.sender].rewardPoints >= _rewardPoints,"Sorry, you do not have sufficient reward points in your account");
         require(etherRewards > _rewardPoints,"Insufficient funds in contract, please try with lesser points or try again later");
@@ -190,12 +222,13 @@ contract MainContract is P2PEnergy{
         prosumers[msg.sender].rewardPoints -= _rewardPoints;
     }
 
+    //Function to deposit ethers in registered prosumer's account
      function deposit() public payable prosumerIsRegistered {
         require(msg.value > 0, "Deposit amount must be greater than zero.");
         prosumers[msg.sender].balance += msg.value;
     }
 
-
+    //Function to withdraw ethers from a registered prosumer's account, provided the energyStatus is not in deficit.
     function withdraw() public prosumerIsRegistered {
             require(prosumers[msg.sender].energyStatus >= 0, "Cannot withdraw funds while energy deficit exists.");
             uint amount = prosumers[msg.sender].balance;
--- finalcontracts.sol
+++ finalcontracts.sol
@@ -1,19 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
+/// @title A peer to peer smart contract with an incentive mechanism
+/// @author Shrusti S. Sangodkar
+/// @dev All function calls are currently implemented without side effects
 
 contract P2PEnergy{
+    uint energyPrice = 1 ether;
+
+    // Struct representing a prosumer
     struct Prosumer{
-        address ID;
-        int energyStatus;
-        uint256 balance;
-        bool isRegistered;
+        address ID; //Stores the address of the prosumer
+        int energyStatus; // Energy status can be positive or negative, indicating the amount of energy the prosumer can supply or needs to buy
+        uint256 balance; // Prosumer's account balance
+        bool isRegistered; // true if prosumer is registered, else false
     }
 
-    mapping(address => Prosumer) internal prosumers;
-    address[] internal buyerQueue;
-    address[] internal sellerQueue;
+    mapping(address => Prosumer) internal prosumers; // Mapping from prosumer addresses to their Prosumer struct
+    address[] internal buyerQueue; // Queue of buyers waiting to buy energy
+    address[] internal sellerQueue; // Queue of sellers waiting to sell energy
+    
+    // Function to register a new prosumer
     function registerProsumer(address _prosumerAddress) internal{
         prosumers[_prosumerAddress] = Prosumer(_prosumerAddress,0,0,true);
     }
+    
+    // Function to remove an element from an array
     function removeAtIndex(uint index, address[] storage array) internal {
         require(index < array.length, "Index out of bounds");
         for (uint i = index; i < array.length - 1; i++) {
@@ -22,23 +33,32 @@ contract P2PEnergy{
         array.pop();
     }
 
+    // Function for a seller to sell energy to a buyer
     function sellEnergy(address _prosumer,int energyUnits) internal {
         bool foundBuyer = false;
+
+        //Iterating through buyer queue to search for buyers
         for(uint i=0; i<buyerQueue.length;i++){
             if(-1*prosumers[buyerQueue[i]].energyStatus <= energyUnits){
-                int y=energyUnits;
+                int actualTransferableEnergy=energyUnits;
+                
+                //Condition to calculate transferable energy is it is less than the available energy units
                 if(-1*prosumers[buyerQueue[i]].energyStatus < energyUnits){
-                    int x=energyUnits + prosumers[buyerQueue[i]].energyStatus;
-                     y= energyUnits - x;
+                    int differenceInEnergy = energyUnits + prosumers[buyerQueue[i]].energyStatus;
+                     actualTransferableEnergy= energyUnits - differenceInEnergy;
+                     
+                      //Store surplus energy in the seller queue
                      Prosumer storage sender = prosumers[_prosumer];
-                     sender.energyStatus = x;
+                     sender.energyStatus = differenceInEnergy ;
                      sellerQueue.push(_prosumer);
                 }
 
-                prosumers[buyerQueue[i]].energyStatus += y;
+                //Transfer of energy to buyer
+                prosumers[buyerQueue[i]].energyStatus += actualTransferableEnergy;
                 
-                prosumers[buyerQueue[i]].balance -= uint(y) * 1 ether;
-                prosumers[_prosumer].balance += uint(y) * 1 ether;
+                // Update the balances of the buyer and the seller
+                prosumers[buyerQueue[i]].balance -= uint(actualTransferableEnergy) * energyPrice;
+                prosumers[_prosumer].balance += uint(actualTransferableEnergy) * energyPrice;
                 foundBuyer = true;
                 if(prosumers[buyerQueue[i]].energyStatus == 0){
                     removeAtIndex(i,buyerQueue);
@@ -47,6 +67,7 @@ contract P2PEnergy{
             }
         }
         if(!foundBuyer){
+            // if no buyer is found, the prosumer is added to the buyer queue
             Prosumer storage sender = prosumers[_prosumer];
             sender.energyStatus += energyUnits;
             sellerQueue.push(_prosumer);
@@ -56,12 +77,17 @@ contract P2PEnergy{
     function buyEnergy(address _prosumer,int energyUnits) internal {
 
         bool foundSeller = false;
+
+        //Iterating through seller queue to search for sellers
         for (uint i = 0; i < sellerQueue.length; i++) {
             if (prosumers[sellerQueue[i]].energyStatus >= -1 * energyUnits) {
+                
+                //Deduct energy from seller (adding here since energyUnits is a negative value)
                 prosumers[sellerQueue[i]].energyStatus += energyUnits;
-                //prosumers[_prosumer].energyStatus -= energyUnits;
-                prosumers[sellerQueue[i]].balance += uint(-energyUnits) * 1 ether;
-                prosumers[_prosumer].balance -= uint(-energyUnits) * 1 ether;
+                
+                // Update the balances of the buyer and the seller                
+                prosumers[sellerQueue[i]].balance += uint(-energyUnits) * energyPrice;
+                prosumers[_prosumer].balance -= uint(-energyUnits) * energyPrice;
                 foundSeller = true;
                 if(prosumers[sellerQueue[i]].energyStatus == 0){
                     removeAtIndex(i,sellerQueue);
@@ -70,6 +96,7 @@ contract P2PEnergy{
             }
         }
         if (!foundSeller) {
+            // if no seller is found, the prosumer is added to the buyer queue
             Prosumer storage sender = prosumers[_prosumer];
             sender.energyStatus += energyUnits;
             buyerQueue.push(_prosumer);
@@ -80,33 +107,41 @@ contract P2PEnergy{
 }
 
 contract MainContract is P2PEnergy{
+
+    //Modifier to check if a prosumer is registered
     modifier prosumerIsRegistered(){
         require(prosumers[msg.sender].isRegistered == true,"The prosumer is not registered");
         _;
     }
 
+    //Modifier to check whether a prosumer is not registered
     modifier notRegisteredProsumer(){
         require(prosumers[msg.sender].isRegistered == false, " The prosumer is already registered");
         _;
     }
 
+    // Modifier to check whether the minimum amount of funds are present in prosumer's account in order to purchase energy units
     modifier checkFunds(int energy){
         uint energyUnits;
         if(energy < 0){
-        energyUnits= uint(-energy);
-        int requestsSent = prosumers[msg.sender].energyStatus;
-        uint256 amountAvailable=prosumers[msg.sender].balance;
-        uint256 amountAvailableRequest=prosumers[msg.sender].balance - uint(-requestsSent) * 1 ether;
-        require(amountAvailable >= energyUnits * 1 ether, " Insufficient balance in prosumer's account");
-        require(amountAvailableRequest >= energyUnits * 1 ether, "Pending request amount exceeds the available balance in prosumer's");
+            energyUnits= uint(-energy);
+            int requestsSent = prosumers[msg.sender].energyStatus;
+            uint256 amountAvailable=prosumers[msg.sender].balance;
+            require(amountAvailable >= energyUnits * energyPrice, " Insufficient balance in prosumer's account");
+
+        //Checking past energy requests and seeing if the pending request amount exceeds the available balance in prosumer's balance
+            uint256 amountAvailableRequest=prosumers[msg.sender].balance - uint(-requestsSent) * energyPrice;
+            require(amountAvailableRequest >= energyUnits * energyPrice, "Pending request amount exceeds the available balance in prosumer's");
         }
         _;
     }
 
+    // Function to register a prosumer
     function register() public notRegisteredProsumer{
         registerProsumer(msg.sender);
     }
 
+    //Function to send a buyer or a seller request 
     function sendEnergyRequest(int energy) public prosumerIsRegistered checkFunds(energy){
        
         if(energy > 0){
@@ -125,12 +160,13 @@ contract MainContract is P2PEnergy{
         return prosumers[msg.sender].balance;
     }
 
+    //Function to deposit ethers in registered prosumer's account
      function deposit() public payable prosumerIsRegistered {
         require(msg.value > 0, "Deposit amount must be greater than zero.");
         prosumers[msg.sender].balance += msg.value;
     }
 
-
+    //Function to withdraw ethers from a registered prosumer's account, provided the energyStatus is not in deficit.
     function withdraw() public prosumerIsRegistered {
             require(prosumers[msg.sender].energyStatus >= 0, "Cannot withdraw funds while energy deficit exists.");
             uint amount = prosumers[msg.sender].balance;
